0d800bc709c4833d324781c5b5e3b02e04f43dac
craftidev <ivann.codev@gmail.com>
2023-12-05 15:00:44 +0100

    course on binding/databinding

--- viewdatabinding/src/main/java/app/craftid/viewdatabinding/MainActivity.kt
+++ viewdatabinding/src/main/java/app/craftid/viewdatabinding/MainActivity.kt
@@ -2,10 +2,23 @@ package app.craftid.viewdatabinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import app.craftid.viewdatabinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+//      val textViewHelloWorld = findViewById<TextView>(R.id.textViewHelloWorld)
+//      val button = findViewById<Button>(R.id.button)
+        val foo = User("Pat", "Bateman", 23, "psycho@killer.co")
+//        binding.textViewAge.text = foo.age
+        binding.user = foo
+        binding.button.setOnClickListener {
+            binding.textViewHelloWorld.text = "Button clicked"
+        }
     }
 }
---
+++ viewdatabinding/src/main/java/app/craftid/viewdatabinding/User.kt
@@ -0,0 +1,8 @@
+package app.craftid.viewdatabinding
+
+data class User(
+    val surname: String,
+    val name: String,
+    val age: Int,
+    val email: String
+)
