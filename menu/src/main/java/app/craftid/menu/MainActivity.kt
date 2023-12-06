65ce5e865997dc8c089596d6d447fb255a34f295
craftidev <ivann.codev@gmail.com>
2023-12-06 16:56:05 +0100

    course on menu

---
+++ menu/src/main/java/app/craftid/menu/MainActivity.kt
@@ -0,0 +1,28 @@
+package app.craftid.menu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.itemAdd -> ""
+            R.id.itemEdit -> ""
+            R.id.itemSettings -> ""
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+}
