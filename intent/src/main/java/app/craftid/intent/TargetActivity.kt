064359975007ae4e6fdc5ae4af430b3b7d2fc145
craftidev <ivann.codev@gmail.com>
2023-12-07 11:16:05 +0100

    use SnackBar instead of Toast, for some reason it doesnt work...

--- intent/src/main/java/app/craftid/intent/TargetActivity.kt
+++ intent/src/main/java/app/craftid/intent/TargetActivity.kt
@@ -2,16 +2,18 @@ package app.craftid.intent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 
 class TargetActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_target)
-        Toast.makeText(
-            this,
-            intent.getStringExtra(EXTRA_STRING_NUMBER),
-            Toast.LENGTH_SHORT
-        ).show()
+        val number = intent.getStringExtra(EXTRA_STRING_NUMBER) ?: "Default value"
+        Log.d("TargetActivity", "Number received: $number")
+//        Toast.makeText(this, number, Toast.LENGTH_SHORT).show()
+        Snackbar.make(findViewById(android.R.id.content), number, Snackbar.LENGTH_LONG).show()
+
     }
 }
